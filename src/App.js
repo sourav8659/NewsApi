c40ec1f673d77a8884c15518c045d30d9ca6388e
sourav8659 <souravghosha92020@gmail.com>
2025-05-28 21:54:58 +0530

    Commit

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ const App=() => {
 				progress={progress}
 				height={3}
 			/>
-			{/* <News pageSize={pageSize} country={country} category="science" /> */}
 			<Route exact path="/" element={<News mode={mode} setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country={country} openedPath="/" />}/>
 			<Route exact path="/business" element={<News mode={mode} setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country={country} category="business" openedPath="/business" />}/>
 			<Route exact path="/entertainment" element={<News mode={mode} setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country={country} category="entertainment" openedPath="/entertainment" />}/>
